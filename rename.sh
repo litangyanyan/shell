48f738d26be3e4b784347ad908626e7873aa8447
liyiheng <liyihenggnehiyil@gmail.com>
2017-07-04 20:51:54 +0800

    Update rename.sh

--- rename.sh
+++ rename.sh
@@ -1,7 +1,10 @@
 dir=$(ls . | grep ".png")
+cnt=1
 for i in $dir
 do
     newI=$(echo $i|tr -d 0-9)
+    newI=${cnt}$(echo $i|tr -d 0-9)
+    cnt=`expr $cnt + 1`
     echo $newI
     echo $i
     $(mv $i $newI)
